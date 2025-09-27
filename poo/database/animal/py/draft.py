5940b03edb9f26617c7d10ab05259e1a3472ae5f
kauas <kauasoussa12345@gmail.com>
2025-09-27 19:01:09 -0300

    animal

---
+++ poo/database/animal/py/draft.py
@@ -0,0 +1,54 @@
+class Animal:
+    def __init__(self, specie: str, sound: str):
+        self.specie: str = specie
+        self.sound: str = sound
+        self.age: int = 0
+
+    def __str__(self) -> str:
+        return f"{self.specie}:{self.age}:{self.sound}"
+
+    def ageBy(self, increment: int) -> None:
+        if self.age == 4:
+            print(f"warnig: {self.specie} morreu")
+            return
+        self.age += increment
+        if self.age >= 4:
+            self.age = 4
+            print(f"warning: {self.specie} morreu")
+
+    def ismakeSound(self) -> str:
+        if self.age == 0:
+            return "---"
+        if self.age == 1:
+            return self.sound
+        if self.age == 2:
+            return self.sound
+        if self.age == 3:
+            return self.sound
+        if self.age == 4:
+            return "RIP"
+
+
+def main():
+    animal: Animal = Animal("", "")
+    while True:
+
+        line:str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
+
+        if args[0] == "end":
+            break
+        if args[0] == "init":
+            specie: str = args[1]
+            sound: str = args[2]
+            animal = Animal(specie, sound)
+        if args[0] == "show":
+            print(animal)
+        if args[0] == "grow":
+            increment: int =(args[1])
+            animal.ageBy(increment)
+        if args[0] == "noise":
+            print(animal.ismakeSound())
+
+main()
